4a611aff394f0a17504eea7ad912a2b74a905b7a
thejasn <thn@redhat.com>
2022-04-26 16:12:36 +0530

    Update makefile: Adds `verify` and updates `lint`

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,10 @@
+//go:build tools
+// +build tools
+
 package tools
 
 import (
 	// Adds targets for makefile
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/openshift/build-machinery-go"
 )
